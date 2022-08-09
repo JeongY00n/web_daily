f1758ac27d31b39906468d48b01331eb7ad8fc00
JeongY00n <shimmumu@gmail.com>
2022-08-09 17:52:20 +0900

    Create 배열고차함수연습하기(3).js

---
+++ 배열고차함수연습하기(3).js
@@ -0,0 +1,17 @@
+	
+let bucketLists = [
+    { id:3, content: '여행가기', completed: false},
+    { id:2, content: '치킨먹기', completed: true},
+    { id:1, content: '코딩하기', completed: false}
+];
+
+function addList(List){
+    bucketLists.reduce(e => 
+        {
+            
+        })
+}
+
+addList({ id:4, content: '요리하기', completed: true})
+
+console.log(bucketLists)
