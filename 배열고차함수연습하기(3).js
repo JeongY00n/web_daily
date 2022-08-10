90b5129ef9feda716186458c017eceda8f7f9b95
JeongY00n <shimmumu@gmail.com>
2022-08-10 13:18:52 +0900

    Update 배열고차함수연습하기(3).js

--- 배열고차함수연습하기(3).js
+++ 배열고차함수연습하기(3).js
@@ -1,4 +1,3 @@
-	
 let bucketLists = [
     { id:3, content: '여행가기', completed: false},
     { id:2, content: '치킨먹기', completed: true},
@@ -6,12 +5,16 @@ let bucketLists = [
 ];
 
 function addList(List){
-    bucketLists.reduce(e => 
+    const newArr = bucketLists.reduce((acc, cur)=>
         {
+            if(!acc[0])
+                acc.push(List)
+            acc.push(cur)
             
-        })
+            return acc
+        },[])
+    console.log(newArr)
 }
 
 addList({ id:4, content: '요리하기', completed: true})
 
-console.log(bucketLists)
